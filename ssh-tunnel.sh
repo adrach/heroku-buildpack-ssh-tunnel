f9ebb71f3516ab8748fb48eb352259d6f9ec3525
Andrew Drach <andrew.drach@yahoo.com>
2017-10-12 23:21:36 -0500

    add the second tunnel configuration and start ssh-agent

--- ssh-tunnel.sh
+++ ssh-tunnel.sh
@@ -1,46 +1,92 @@
 #!/bin/bash
 
-[ -v SSHTUNNEL_REMOTE_PORT ] || SSHTUNNEL_REMOTE_PORT=22
+[ -v SSHTUNNEL_REMOTE_PORT1 ] || SSHTUNNEL_REMOTE_PORT1=22
+[ -v SSHTUNNEL_REMOTE_PORT2 ] || SSHTUNNEL_REMOTE_PORT2=22
 
 function log {
   echo "ssh-tunnel	event=$1"
 }
 
-function is_configured {
+function is_configured1 {
   [[ \
-    -v SSHTUNNEL_PRIVATE_KEY && \
-    -v SSHTUNNEL_TUNNEL_CONFIG && \
-    -v SSHTUNNEL_REMOTE_USER && \
-    -v SSHTUNNEL_REMOTE_HOST
+    -v SSHTUNNEL_PRIVATE_KEY1 && \
+    -v SSHTUNNEL_TUNNEL_CONFIG1 && \
+    -v SSHTUNNEL_REMOTE_USER1 && \
+    -v SSHTUNNEL_REMOTE_HOST1
   ]] && return 0 || return 1
 }
 
-function deploy_key {
+function is_configured2 {
+  [[ \
+    -v SSHTUNNEL_PRIVATE_KEY2 && \
+    -v SSHTUNNEL_TUNNEL_CONFIG2 && \
+    -v SSHTUNNEL_REMOTE_USER2 && \
+    -v SSHTUNNEL_REMOTE_HOST2
+  ]] && return 0 || return 1
+}
+
+function deploy_key1 {
   mkdir -p ${HOME}/.ssh
   chmod 700 ${HOME}/.ssh
 
-  echo "${SSHTUNNEL_PRIVATE_KEY}" > ${HOME}/.ssh/ssh-tunnel-key
-  chmod 600 ${HOME}/.ssh/ssh-tunnel-key
+  echo "${SSHTUNNEL_PRIVATE_KEY1}" > ${HOME}/.ssh/ssh-tunnel-key1
+  chmod 600 ${HOME}/.ssh/ssh-tunnel-key1
 
-  ssh-keyscan -p ${SSHTUNNEL_REMOTE_PORT} ${SSHTUNNEL_REMOTE_HOST} > ${HOME}/.ssh/known_hosts
+  ssh-keyscan -p ${SSHTUNNEL_REMOTE_PORT1} ${SSHTUNNEL_REMOTE_HOST1} >> ${HOME}/.ssh/known_hosts
+  ssh-add ${HOME}/.ssh/ssh-tunnel-key1
+  log "Added key 1"
 }
 
-function spawn_tunnel {
+function deploy_key2 {
+  mkdir -p ${HOME}/.ssh
+  chmod 700 ${HOME}/.ssh
+
+  echo "${SSHTUNNEL_PRIVATE_KEY2}" > ${HOME}/.ssh/ssh-tunnel-key2
+  chmod 600 ${HOME}/.ssh/ssh-tunnel-key2
+
+  ssh-keyscan -p ${SSHTUNNEL_REMOTE_PORT2} ${SSHTUNNEL_REMOTE_HOST2} >> ${HOME}/.ssh/known_hosts
+  ssh-add ${HOME}/.ssh/ssh-tunnel-key2
+  log "Added key 2"
+}
+
+function spawn_tunnel1 {
   while true; do
-    log "ssh-connection-init"
-    ssh -i ${HOME}/.ssh/ssh-tunnel-key -N -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -L ${SSHTUNNEL_TUNNEL_CONFIG} ${SSHTUNNEL_REMOTE_USER}@${SSHTUNNEL_REMOTE_HOST} -p ${SSHTUNNEL_REMOTE_PORT}
-    log "ssh-connection-end"
+    log "ssh-connection-init 1"
+    ssh -i ${HOME}/.ssh/ssh-tunnel-key1 -N -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -L ${SSHTUNNEL_TUNNEL_CONFIG1} ${SSHTUNNEL_REMOTE_USER1}@${SSHTUNNEL_REMOTE_HOST1} -p ${SSHTUNNEL_REMOTE_PORT1}
+    log "ssh-connection-end 1"
+    sleep 5;
+  done &
+}
+
+function spawn_tunnel2 {
+  while true; do
+    log "ssh-connection-init 2"
+    ssh -i ${HOME}/.ssh/ssh-tunnel-key2 -N -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -L ${SSHTUNNEL_TUNNEL_CONFIG2} ${SSHTUNNEL_REMOTE_USER2}@${SSHTUNNEL_REMOTE_HOST2} -p ${SSHTUNNEL_REMOTE_PORT2}
+    log "ssh-connection-end 2"
     sleep 5;
   done &
 }
 
 log "starting"
+echo " " > ${HOME}/.ssh/known_hosts
 
-if is_configured; then
-  deploy_key
-  spawn_tunnel
+log "Starting SSH agent"
+eval $(ssh-agent)
 
-  log "spawned";
+if is_configured1; then
+  deploy_key1
+  spawn_tunnel1
+
+  log "spawned 1";
 else
-  log "missing-configuration"
+  log "missing-configuration 1"
+fi
+
+if is_configured2; then
+  deploy_key2
+  spawn_tunnel2
+
+  log "spawned 2";
+else
+  log "missing-configuration 2"
 fi
